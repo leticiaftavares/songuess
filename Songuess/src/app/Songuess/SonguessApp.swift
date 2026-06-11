8a1e88ccbc8ec3f7b3830b3ceefff97851d612b9
marcelodantasf <mdantasfilho2005@gmail.com>
2026-06-11 10:53:23 -0300

    Initial Commit

---
+++ Songuess/src/app/Songuess/SonguessApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonguessApp.swift
+//  Songuess
+//
+//  Created by Marcelo Dantas on 11/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SonguessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Songuess/src/app/SonguessTV/SonguessTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  SonguessTVApp.swift
+//  SonguessTV
+//
+//  Created by Marcelo Dantas on 11/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SonguessTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
